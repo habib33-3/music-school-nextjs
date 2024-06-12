9a351f5c477a67505f2bdc1a4200885881649ac0
habib33-3 <habib.swi16@gmail.com>
2024-06-12 15:20:20 +0600

    featured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 const HomePage = () => {
   return (
     <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.02]">
-     <HeroSection/>
+      <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 };
