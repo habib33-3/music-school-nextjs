b629f9e89b62dcee595cce1ccf7dc19ddc6fa4e4
habib33-3 <habib.swi16@gmail.com>
2024-06-12 14:19:32 +0600

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 const HomePage = () => {
   return (
     <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-center text-2xl">Music</h1>
+     <HeroSection/>
     </main>
   );
 };
