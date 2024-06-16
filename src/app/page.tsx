f95427033f509dae97bad561b176b83fba07445a
habib33-3 <habib.swi16@gmail.com>
2024-06-16 18:42:23 +0600

    why choose us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 const HomePage = () => {
   return (
     <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 };
